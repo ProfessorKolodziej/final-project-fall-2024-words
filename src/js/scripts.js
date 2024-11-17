91d8ff3ce367d972644b09eee806da003c98cac0
Ziyi-C-1221 <zycheng@bu.edu>
2024-11-16 21:01:56 -0500

    html audio

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,8 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+function playAudio(audioId) {
+	const audioElement = document.getElementById(audioId);
+	audioElement.play();
+}
